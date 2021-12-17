c3954d3f4259735408167bed53bd661be3df6b2d
Jack Michaud <jack@lomz.me>
2021-12-16 21:55:28 -0800

    feat(nbandit): add nonstationary bandit

--- nbandit/bandit.py
+++ nbandit/bandit.py
@@ -21,3 +21,34 @@ def generate_bandit(n):
     return [generate_lever() for _ in range(n)]
 
 
+def generate_nonstationary_lever():
+    """
+    Creates a lever; a function that returns a reward.
+    The reward returned is a normal distribution around a center with a variance of 1.
+    The center of the reward for this lever is initially a constant (5) but will take random walks and change value.
+    The center of the reward will only ever be between [0,9].
+    """
+    global action_value
+    action_value = 5
+    step_size = 0.5
+
+    def return_reward():
+        global action_value
+        action_value = np.random.choice(
+            [
+                min(9, action_value + step_size),
+                max(0, action_value - step_size),
+                action_value,
+            ]
+        )
+        return np.random.normal(action_value, 1)
+
+    def get_action_value():
+        global action_value
+        return action_value + 1
+
+    return get_action_value, return_reward
+
+
+def generate_nonstationary_bandit(n):
+    return [generate_nonstationary_lever() for _ in range(n)]
